07e7d3058769195462ae70008326d05cce37a077
Louis <pm6422@126.com>
2018-08-02 13:56:22 +0800

    refactor

--- passport-server/src/main/java/org/infinity/passport/security/AjaxLogoutSuccessHandler.java
+++ passport-server/src/main/java/org/infinity/passport/security/AjaxLogoutSuccessHandler.java
@@ -1,11 +1,5 @@
 package org.infinity.passport.security;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -14,6 +8,9 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.web.authentication.AbstractAuthenticationTargetUrlRequestHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Spring Security logout handler, specialized for Ajax requests.
@@ -26,8 +23,7 @@ public class AjaxLogoutSuccessHandler extends AbstractAuthenticationTargetUrlReq
     private TokenStore tokenStore;
 
     @Override
-    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
-            throws IOException, ServletException {
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         // Remove the access token
         String token = request.getHeader("authorization");
         if (token != null && token.toLowerCase().startsWith(OAuth2AccessToken.BEARER_TYPE.toLowerCase())) {
