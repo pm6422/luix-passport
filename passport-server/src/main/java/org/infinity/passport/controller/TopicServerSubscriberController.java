ea8853cfd44f7bf96987a98f88229b71c2e1d8e7
louis <louisliu@deepbrain.ai>
2018-08-27 15:12:38 +0800

    add tracker page

--- passport-server/src/main/java/org/infinity/passport/controller/TopicServerSubscriberController.java
+++ passport-server/src/main/java/org/infinity/passport/controller/TopicServerSubscriberController.java
@@ -4,11 +4,13 @@ import org.infinity.passport.dto.TrackerDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import java.security.Principal;
 import java.time.Instant;
@@ -16,28 +18,28 @@ import java.time.Instant;
 import static org.infinity.passport.config.WebsocketConfiguration.IP_ADDRESS;
 
 @Controller
-public class TopicServerSubscriberController {
+public class TopicServerSubscriberController implements ApplicationListener<SessionDisconnectEvent> {
     private static final Logger                LOGGER = LoggerFactory.getLogger(TopicServerSubscriberController.class);
     @Autowired
     private              SimpMessagingTemplate messagingTemplate;
 
-
-    //    @Override
-    //    public void onApplicationEvent(SessionDisconnectEvent event) {
-    //        ActivityDTO activityDTO = new ActivityDTO();
-    //        activityDTO.setSessionId(event.getSessionId());
-    //        activityDTO.setPage("logout");
-    //        messagingTemplate.convertAndSend("/topic/client-subscriber/tracker", activityDTO);
-    //    }
-
-    @SubscribeMapping("/topic/server-subscriber/tracker")
-    //    @SendTo("/topic/client-subscriber/tracker") @SendTo会被@Aspect拦截导致无效
-    public void sendActivity(@Payload TrackerDTO activityDTO, StompHeaderAccessor stompHeaderAccessor, Principal principal) {
-        activityDTO.setUserLogin(principal.getName());
-        activityDTO.setSessionId(stompHeaderAccessor.getSessionId());
-        activityDTO.setIpAddress(stompHeaderAccessor.getSessionAttributes().get(IP_ADDRESS).toString());
-        activityDTO.setTime(Instant.now());
-        LOGGER.debug("Sending user tracking data {}", activityDTO);
+    @Override
+    public void onApplicationEvent(SessionDisconnectEvent event) {
+        TrackerDTO activityDTO = new TrackerDTO();
+        activityDTO.setSessionId(event.getSessionId());
+        activityDTO.setPage("logout");
         messagingTemplate.convertAndSend("/topic/client-subscriber/tracker", activityDTO);
     }
+
+    @MessageMapping("/topic/server-subscriber/tracker")
+    //    @SendTo("/topic/client-subscriber/tracker") @SendTo会被@Aspect拦截导致无效
+    public void sendActivity(@Payload TrackerDTO trackerDTO, StompHeaderAccessor stompHeaderAccessor, Principal principal) {
+        trackerDTO.setUserLogin(principal.getName());
+        trackerDTO.setSessionId(stompHeaderAccessor.getSessionId());
+        trackerDTO.setIpAddress(stompHeaderAccessor.getSessionAttributes().get(IP_ADDRESS).toString());
+        trackerDTO.setTime(Instant.now());
+        LOGGER.debug("Sending user tracking data {}", trackerDTO);
+        messagingTemplate.convertAndSend("/topic/client-subscriber/tracker", trackerDTO);
+    }
+
 }
