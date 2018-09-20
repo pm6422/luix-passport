b4c67989187c68bdfb0680c20a07e2f388d52553
louis <louisliu@deepbrain.ai>
2018-09-20 11:37:09 +0800

    reset database

--- passport-server/src/main/resources/templates/constants.js
+++ passport-server/src/main/resources/templates/constants.js
@@ -6,7 +6,7 @@
         .module('smartcloudserviceApp')
         .constant('APP_NAME', '@project.name@')
         .constant('VERSION', '@project.version@')
-        .constant('COMPANY_NAME', '义语智能科技（上海）有限公司')
+        .constant('COMPANY_NAME', 'Infinity Organization')
         .constant('PAGINATION_CONSTANTS', {
             'itemsPerPage': 10
          })
