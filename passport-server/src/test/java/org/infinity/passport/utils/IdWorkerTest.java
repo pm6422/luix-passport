b23b52e569af437f94b8ed012362337ec87d09a0
louis <louisliu@deepbrain.ai>
2018-10-31 11:25:18 +0800

    new project

---
+++ passport-server/src/test/java/org/infinity/passport/utils/IdWorkerTest.java
@@ -0,0 +1,53 @@
+package org.infinity.passport.utils;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.IntStream;
+
+class IdWorkerThread implements Callable<Long> {
+    private IdWorker       idWorker;
+    private CountDownLatch countDownLatch;
+
+    public IdWorkerThread(IdWorker idWorker, CountDownLatch countDownLatch) {
+        this.idWorker = idWorker;
+        this.countDownLatch = countDownLatch;
+    }
+
+    @Override
+    public Long call() {
+        long l = idWorker.nextId();
+        countDownLatch.countDown();
+        return l;
+    }
+}
+
+public class IdWorkerTest {
+    @Test
+    public void testConcurrency() throws InterruptedException {
+        int threadNum = 1000;
+        List<Future<Long>> futures = new ArrayList<>(threadNum);
+        List<Long> results = new ArrayList<>(threadNum);
+        IdWorker idWorker = new IdWorker(10, 10);
+        ExecutorService threadPool = Executors.newFixedThreadPool(100);
+        CountDownLatch countDownLatch = new CountDownLatch(threadNum);
+        IntStream.range(0, threadNum).forEach(i -> {
+            Future<Long> future = threadPool.submit(new IdWorkerThread(idWorker, countDownLatch));
+            futures.add(future);
+        });
+        countDownLatch.await();
+        futures.forEach(future -> {
+            try {
+                Long result = future.get();
+                results.add(result);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println(results.stream().distinct().count());
+    }
+}
