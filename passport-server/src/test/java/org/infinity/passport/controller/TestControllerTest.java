fe18d6fed55ffbd34f6ec37738d472372300e28e
louis <louisliu@deepbrain.ai>
2019-02-08 14:42:08 +0800

    refactor

--- passport-server/src/test/java/org/infinity/passport/controller/TestControllerTest.java
+++ passport-server/src/test/java/org/infinity/passport/controller/TestControllerTest.java
@@ -3,15 +3,22 @@ package org.infinity.passport.controller;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.StopWatch;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
@@ -19,10 +26,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @RunWith(SpringRunner.class)
 public class TestControllerTest {
 
+    private static final Logger                LOGGER = LoggerFactory.getLogger(TestControllerTest.class);
     @Autowired
-    private WebApplicationContext context;
-
-    private MockMvc mockMvc;
+    private              WebApplicationContext context;
+    private              MockMvc               mockMvc;
 
     @Before
     public void setUp() {
@@ -30,16 +37,31 @@ public class TestControllerTest {
     }
 
     @Test
-    public void testThreadSafe() {
-        for (int i = 0; i < 1000; i++) {
-            String key = UUID.randomUUID().toString().replaceAll("-", "");
-            new Thread(() -> {
+    public void testThreadSafe() throws InterruptedException {
+        StopWatch watch = new StopWatch();
+        watch.start();
+        int requestCount = 1000;
+        int threadPoolSize = 20;
+        ExecutorService threadPool = Executors.newFixedThreadPool(threadPoolSize);
+
+        IntStream.range(0, requestCount).forEach(i -> {
+            threadPool.execute(() -> {
+                LOGGER.debug("Active thread count: {}", Thread.activeCount());
+                String key = UUID.randomUUID().toString().replaceAll("-", "");
                 try {
                     this.mockMvc.perform(get("/open-api/test/threadsafe?key=" + key).accept(MediaType.APPLICATION_JSON));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-            }).start();
+            });
+        });
+
+        threadPool.shutdown();
+        if (threadPool.awaitTermination(2, TimeUnit.SECONDS)) {
+            watch.stop();
+            LOGGER.debug("Total: {} s", watch.getTotalTimeMillis() / 1000);
+            LOGGER.debug("Mean: {} ms", watch.getTotalTimeMillis() / requestCount);
+            LOGGER.debug("TPS: {}", requestCount / (watch.getTotalTimeMillis() / 1000));
         }
     }
 }
