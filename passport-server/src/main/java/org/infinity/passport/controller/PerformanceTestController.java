98f5fffc0b1e9ad5bb6e5bc2f7db5c025fc70ae3
louis <louisliu@deepbrain.ai>
2019-02-11 14:40:46 +0800

    refactor

--- passport-server/src/main/java/org/infinity/passport/controller/PerformanceTestController.java
+++ passport-server/src/main/java/org/infinity/passport/controller/PerformanceTestController.java
@@ -27,16 +27,14 @@ public class PerformanceTestController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PerformanceTestController.class);
 
-    private static final Set<String>        PARAM_SET = new ConcurrentSkipListSet<>();// 存储已有参数，用于判断参数是否重复，从而判断线程是否安全
+    private static final Set<String> PARAM_SET = new ConcurrentSkipListSet<>();// 存储已有参数，用于判断参数是否重复，从而判断线程是否安全
     @Autowired
-    private              Environment        env;
+    private              Environment env;
     // 自动注入request，Spring并没有注入一个request对象，而是注入了一个代理（proxy）；当Bean中需要使用request对象时，通过该代理获取request对象。
     // 代理的实现参见AutowireUtils的内部类ObjectFactoryDelegatingInvocationHandler
     // 当我们调用request的方法method时，实际上是调用了由objectFactory.getObject()生成的对象的method方法；objectFactory.getObject()生成的对象才是真正的request对象。
     // 发现objectFactory的类型为WebApplicationContextUtils的内部类RequestObjectFactory
     // 通过代码可以看出，生成的RequestAttributes对象是线程局部变量（ThreadLocal），因此request对象也是线程局部变量；这就保证了request对象的线程安全性。
-    @Autowired
-    private              HttpServletRequest request;
 
     @GetMapping("/open-api/performance/req-thread-safe")
     public void test(HttpServletRequest request) throws InterruptedException {
@@ -73,5 +71,12 @@ public class PerformanceTestController {
             }.start();
         }
         return ResponseEntity.ok(null);
+    }
+
+    @ApiOperation(value = "死循环")
+    @GetMapping("/open-api/performance/loop")
+    public void loop() {
+        while (true) {
+        }
     }
 }
