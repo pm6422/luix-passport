a76c558054e2eb097d4fc0a551f9ad69b6b1d692
louis <louisliu@deepbrain.ai>
2019-04-26 10:53:03 +0800

    refactor

--- passport-server/src/main/java/org/infinity/passport/config/ServiceMetricsAspect.java
+++ passport-server/src/main/java/org/infinity/passport/config/ServiceMetricsAspect.java
@@ -3,7 +3,6 @@ package org.infinity.passport.config;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -22,11 +21,7 @@ public class ServiceMetricsAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ServiceMetricsAspect.class);
 
-    @Pointcut("within(" + ApplicationConstants.BASE_PACKAGE + ".service.impl.*)")
-    public void servicePointcut() {
-    }
-
-    @Around("servicePointcut()")
+    @Around("within(" + ApplicationConstants.BASE_PACKAGE + ".service.impl.*)")
     public Object serviceAround(ProceedingJoinPoint joinPoint) throws Throwable {
         try {
             StopWatch stopWatch = new StopWatch();
