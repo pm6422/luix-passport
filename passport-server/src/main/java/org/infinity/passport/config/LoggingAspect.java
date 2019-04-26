d4818081beec7ab93b04f41e115b8e8bdbd65116
louis <louisliu@deepbrain.ai>
2019-04-26 10:53:42 +0800

    refactor

--- passport-server/src/main/java/org/infinity/passport/config/LoggingAspect.java
+++ passport-server/src/main/java/org/infinity/passport/config/LoggingAspect.java
@@ -3,7 +3,6 @@ package org.infinity.passport.config;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -23,12 +22,7 @@ public class LoggingAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Pointcut("within(" + ApplicationConstants.BASE_PACKAGE + ".service..*) || within("
-            + ApplicationConstants.BASE_PACKAGE + ".controller..*)")
-    public void loggingPointcut() {
-    }
-
-    @Around("loggingPointcut()")
+    @Around("within(" + ApplicationConstants.BASE_PACKAGE + ".service..*) || within(" + ApplicationConstants.BASE_PACKAGE + ".controller..*)")
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
         if (LOGGER.isDebugEnabled()) {
             LOGGER.debug("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
