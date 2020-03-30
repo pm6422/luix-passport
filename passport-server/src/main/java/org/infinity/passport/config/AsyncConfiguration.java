22580ae775d15f976465eff979635d96f02b287c
louis <pm6422@126.com>
2020-03-30 16:13:44 +0800

    upgrade to springboot 2.2.5.RELEASE

--- passport-server/src/main/java/org/infinity/passport/config/AsyncConfiguration.java
+++ passport-server/src/main/java/org/infinity/passport/config/AsyncConfiguration.java
@@ -1,41 +1,39 @@
 package org.infinity.passport.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.infinity.passport.async.ExceptionHandlingAsyncTaskExecutor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
 @EnableScheduling
+@Slf4j
 public class AsyncConfiguration implements AsyncConfigurer {
 
-    private static final Logger   LOGGER = LoggerFactory.getLogger(AsyncConfiguration.class);
-
     @Autowired
-    private ApplicationProperties applicationProperties;
+    private              TaskExecutionProperties taskExecutionProperties;
 
     @Override
     @Bean(name = "asyncTaskExecutor")
     public Executor getAsyncExecutor() {
-        LOGGER.debug("Creating Async Task Executor");
+        log.debug("Creating Async Task Executor");
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(applicationProperties.getAsync().getCorePoolSize());
-        executor.setMaxPoolSize(applicationProperties.getAsync().getMaxPoolSize());
-        executor.setQueueCapacity(applicationProperties.getAsync().getQueueCapacity());
-        executor.setThreadNamePrefix(ApplicationConstants.BASE_PACKAGE + ".asyn.executor-");
-        LOGGER.debug("Created Async Task Executor");
+        executor.setCorePoolSize(taskExecutionProperties.getPool().getCoreSize());
+        executor.setMaxPoolSize(taskExecutionProperties.getPool().getMaxSize());
+        executor.setQueueCapacity(taskExecutionProperties.getPool().getQueueCapacity());
+        executor.setThreadNamePrefix(taskExecutionProperties.getThreadNamePrefix());
+        log.debug("Created Async Task Executor");
         return new ExceptionHandlingAsyncTaskExecutor(executor);
     }
 
@@ -44,14 +42,14 @@ public class AsyncConfiguration implements AsyncConfigurer {
         return new SimpleAsyncUncaughtExceptionHandler();
     }
 
-    @Bean
-    public ThreadPoolTaskScheduler taskScheduler() {
-        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
-        taskScheduler.setPoolSize(10);
-        taskScheduler.setRemoveOnCancelPolicy(true);
-        taskScheduler.setErrorHandler(t -> LOGGER.error("Unexpected error occurred in scheduled task.", t));
-        taskScheduler.setBeanName(applicationProperties.getCache().getCachePrefix() + "thread");
-        taskScheduler.initialize();
-        return taskScheduler;
-    }
+//    @Bean
+//    public ThreadPoolTaskScheduler taskScheduler() {
+//        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+//        taskScheduler.setPoolSize(10);
+//        taskScheduler.setRemoveOnCancelPolicy(true);
+//        taskScheduler.setErrorHandler(t -> log.error("Unexpected error occurred in scheduled task.", t));
+//        taskScheduler.setBeanName(applicationProperties.getCache().getCachePrefix() + "thread");
+//        taskScheduler.initialize();
+//        return taskScheduler;
+//    }
 }
