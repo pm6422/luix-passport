5b18566dda543831e4ef255f5f226f71c532c377
louis <pm6422@126.com>
2020-03-30 15:45:44 +0800

    upgrade to springboot 2.2.5.RELEASE

--- passport-server/src/main/java/org/infinity/passport/domain/PersistentAuditEvent.java
+++ passport-server/src/main/java/org/infinity/passport/domain/PersistentAuditEvent.java
@@ -1,40 +1,46 @@
 package org.infinity.passport.domain;
 
+ import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
+ import org.springframework.data.mongodb.core.index.Indexed;
+ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Date;
-import java.util.HashMap;
+ import java.util.Date;
+ import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Persist AuditEvent managed by the Spring Boot actuator @see org.springframework.boot.actuate.audit.AuditEvent
  */
 @Document(collection = "PersistentAuditEvent")
+@EqualsAndHashCode
+@ToString
 public class PersistentAuditEvent implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Id
-    @Field("event_id")
     private String id;
 
     @NotNull
     private String principal;
 
+    @Field("event_date")
     private Instant auditEventDate;
-    @Field("event_type")
-    private String  auditEventType;
 
-    private Map<String, String> data = new HashMap<>();
+    @Field("event_type")
+    private String auditEventType;
 
     @Indexed(expireAfterSeconds = 0)//Expire Documents at a Specific Clock Time
     private Date expiration;
+
+    private Map<String, String> data = new HashMap<>();
 
     public String getId() {
         return id;
