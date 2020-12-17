ef9fc31659f7cf6c67571635b86058e57e8a332e
louis <pm6422@126.com>
2020-12-17 14:25:48 +0800

    refactor error handling

---
+++ passport-server/src/main/java/org/infinity/passport/config/BeanValidatorConfiguration.java
@@ -0,0 +1,23 @@
+package org.infinity.passport.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class BeanValidatorConfiguration {
+
+    private final MessageSource messageSource;
+
+    public BeanValidatorConfiguration(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        LocalValidatorFactoryBean validatorFactoryBean = new LocalValidatorFactoryBean();
+        validatorFactoryBean.setValidationMessageSource(messageSource);
+        return validatorFactoryBean;
+    }
+}
