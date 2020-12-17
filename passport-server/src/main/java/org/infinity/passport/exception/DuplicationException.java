65577d98d59750e0b275fa61628831db959dc595
louis <pm6422@126.com>
2020-12-17 14:20:17 +0800

    refactor error handling

--- passport-server/src/main/java/org/infinity/passport/exception/DuplicationException.java
+++ passport-server/src/main/java/org/infinity/passport/exception/DuplicationException.java
@@ -1,10 +1,19 @@
 package org.infinity.passport.exception;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Map;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class DuplicationException extends RuntimeException {
 
-    private static final long serialVersionUID = 4161299998151198599L;
+    private static final long                serialVersionUID = 4161299998151198599L;
+    private final        Map<String, Object> fieldMap;
 
-    public DuplicationException(String message) {
-        super(message);
+    public DuplicationException(Map<String, Object> fieldMap) {
+        super("Found duplicated data!");
+        this.fieldMap = fieldMap;
     }
 }
