7c4121e3d42a1a1c55ace9de4d4d2f45759a725f
louis <pm6422@126.com>
2021-02-05 20:26:08 +0800

    refactor

--- passport-server/src/main/java/org/infinity/passport/config/AopLoggingAspect.java
+++ passport-server/src/main/java/org/infinity/passport/config/AopLoggingAspect.java
@@ -103,9 +103,6 @@ public class AopLoggingAspect {
     }
 
     private boolean matchLogMethod(ProceedingJoinPoint joinPoint) {
-        if (!applicationProperties.getAopLogging().isEnabled()) {
-            return false;
-        }
         if (!applicationProperties.getAopLogging().isMethodWhitelistMode()) {
             return true;
         }
