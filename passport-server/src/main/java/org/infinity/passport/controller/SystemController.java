aa1757d1ebb91d00cf68037eab3db8c9410c66f3
刘尧 <liuyao@xforceplus.com>
2021-04-28 22:01:22 +0800

    refactor

--- passport-server/src/main/java/org/infinity/passport/controller/SystemController.java
+++ passport-server/src/main/java/org/infinity/passport/controller/SystemController.java
@@ -2,16 +2,20 @@ package org.infinity.passport.controller;
 
 import io.changock.runner.core.ChangockBase;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.infinity.passport.config.ApplicationProperties;
 import org.infinity.passport.domain.Authority;
 import org.infinity.passport.utils.NetworkUtils;
+import org.springframework.context.ApplicationContext;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -20,14 +24,19 @@ import java.io.IOException;
 @Slf4j
 public class SystemController {
 
-    private final ApplicationProperties applicationProperties;
-    private final ChangockBase          changockBase;
-    private final MongoTemplate         mongoTemplate;
+    @Resource
+    private ApplicationProperties applicationProperties;
+    @Resource
+    private ChangockBase          changockBase;
+    @Resource
+    private MongoTemplate         mongoTemplate;
+    @Resource
+    private ApplicationContext    applicationContext;
 
-    public SystemController(ApplicationProperties applicationProperties, ChangockBase changockBase, MongoTemplate mongoTemplate) {
-        this.applicationProperties = applicationProperties;
-        this.changockBase = changockBase;
-        this.mongoTemplate = mongoTemplate;
+    @ApiOperation("get bean")
+    @GetMapping("/api/system/bean")
+    public ResponseEntity<Object> getBean(@RequestParam(value = "name") String name) {
+        return ResponseEntity.ok(applicationContext.getBean(name));
     }
 
     @GetMapping("/api/system/redis-admin")
