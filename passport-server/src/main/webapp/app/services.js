8bb2e608125e877708b4d8918ec2e8ce40d2e33d
刘尧 <liuyao@xforceplus.com>
2021-08-14 18:42:36 +0800

    refactor url

--- passport-server/src/main/webapp/app/services.js
+++ passport-server/src/main/webapp/app/services.js
@@ -496,7 +496,7 @@ function MetricsService($rootScope, $http) {
     };
 
     function getMetrics() {
-        return $http.get('api/metric/metrics').then(function (response) {
+        return $http.get('api/metrics').then(function (response) {
             return response.data;
         });
     }
