4a6c3f2b39408835cef1d1033c9ffd86c3509db8
刘尧 <liuyao@xforceplus.com>
2021-08-19 22:29:31 +0800

    refactor thread pool

--- passport-server/src/main/java/org/infinity/passport/filter/SsoCorsFilter.java
+++ passport-server/src/main/java/org/infinity/passport/filter/SsoCorsFilter.java
@@ -16,8 +16,7 @@ import java.io.IOException;
 public class SsoCorsFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-            throws IOException, ServletException {
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
         log.debug("Access URL: {}", request.getRequestURI());
