a9a6964abf069fb11252726aca3906716b3e3d0a
刘尧 <liuyao@xforceplus.com>
2022-06-18 15:00:25 +0800

    refactor

--- passport-server/src/main/java/org/infinity/passport/aspect/AopLoggingAspect.java
+++ passport-server/src/main/java/org/infinity/passport/aspect/AopLoggingAspect.java
@@ -1,6 +1,6 @@
 package org.infinity.passport.aspect;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
