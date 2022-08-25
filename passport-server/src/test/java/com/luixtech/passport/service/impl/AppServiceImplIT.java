782c8edda17ac10fb2f213c22947b6639a2f4add
刘尧 <liuyao@xforceplus.com>
2022-08-25 21:06:59 +0800

    refactor

---
+++ passport-server/src/test/java/com/luixtech/passport/service/impl/AppServiceImplIT.java
@@ -0,0 +1,35 @@
+package com.luixtech.passport.service.impl;
+
+import com.luixtech.passport.IntegrationTest;
+import com.luixtech.passport.domain.App;
+import com.luixtech.passport.domain.AppAuthority;
+import com.luixtech.passport.repository.AppRepository;
+import com.luixtech.passport.service.AppService;
+import com.luixtech.uidgenerator.core.id.IdGenerator;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.annotation.Resource;
+import java.util.Set;
+
+@IntegrationTest
+@Slf4j
+public class AppServiceImplIT {
+
+    @Resource
+    private AppService    appService;
+    @Resource
+    private AppRepository appRepository;
+
+    @Test
+    @DisplayName("insert app and authorities")
+    public void insert() throws Exception {
+        String appName = IdGenerator.generateTraceId();
+        App app = new App();
+        app.setName(appName);
+        app.setAuthorities(Set.of(new AppAuthority(null, "ROLE_ADMIN")));
+        appService.insert(app);
+        appRepository.deleteById(app.getId());
+    }
+}
