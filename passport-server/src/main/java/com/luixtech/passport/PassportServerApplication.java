b4e7527cde1462a71e55f5129bf04de5cc4b2f79
刘尧 <liuyao@xforceplus.com>
2022-08-26 20:09:54 +0800

    refactor

--- passport-server/src/main/java/com/luixtech/passport/PassportServerApplication.java
+++ passport-server/src/main/java/com/luixtech/passport/PassportServerApplication.java
@@ -1,29 +1,17 @@
 package com.luixtech.passport;
 
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.ArrayUtils;
-import com.luixtech.passport.config.ApplicationConstants;
 import com.luixtech.passport.config.ApplicationProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.core.env.Environment;
-import org.springframework.util.Assert;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-import java.util.Arrays;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ApplicationProperties.class})
 @ServletComponentScan
 @Slf4j
 public class PassportServerApplication {
-
-    @Resource
-    private Environment env;
-
     /**
      * Entrance method which used to run the application. Spring profiles can be configured with a program arguments
      * --spring.profiles.active=your-active-profile
@@ -32,17 +20,5 @@ public class PassportServerApplication {
      */
     public static void main(String[] args) {
         SpringApplication.run(PassportServerApplication.class, args);
-    }
-
-    @PostConstruct
-    private void validateProfiles() {
-        Assert.notEmpty(env.getActiveProfiles(), "No Spring profile configured!");
-        Assert.isTrue(env.getActiveProfiles().length == 1, "Multiple profiles are not allowed!");
-        Arrays.stream(env.getActiveProfiles())
-                .filter(activeProfile -> !ArrayUtils.contains(ApplicationConstants.AVAILABLE_PROFILES, activeProfile))
-                .findFirst().ifPresent((activeProfile) -> {
-                    log.error("Mis-configured application with an illegal profile '{}'!", activeProfile);
-                    System.exit(0);
-                });
     }
 }
