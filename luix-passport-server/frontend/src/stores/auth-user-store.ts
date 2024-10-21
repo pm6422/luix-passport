704317dd78c5e1f1e25282c570e1d1a630250441
pm6422 <pm6422@126.com>
2024-10-21 16:06:32 +0200

    refactor

--- luix-passport-server/frontend/src/stores/auth-user-store.ts
+++ luix-passport-server/frontend/src/stores/auth-user-store.ts
@@ -14,65 +14,20 @@ export type AuthUser = {
     activated: boolean
     enabled: boolean
     roles: Array<string>
+    isAdmin: boolean
+    isDeveloper: boolean
+    isOnlyUser: boolean
 }
 
 class AuthUserStore extends Exome {
-    public id = ""
-    public username = ""
-    public email = ""
-    public firstName = ""
-    public lastName = ""
-    public locale = "en_US"
-    public timeZone = "Asia/Shanghai"
-    public dateTimeFormat = ""
-    public dateTimeFormatInstance = {}
-    public activated = false
-    public enabled = false
-    public roles: Array<string> = []
-
+    public authUser = {} as AuthUser
 
     public setAuthUser(authUser: AuthUser) {
-        this.id = authUser.id
-        this.username = authUser.username
-        this.email = authUser.email
-        this.firstName = authUser.firstName
-        this.lastName = authUser.lastName
-        this.locale = authUser.locale
-        this.timeZone = authUser.timeZone
-        this.dateTimeFormat = authUser.dateTimeFormat
-        this.dateTimeFormatInstance = authUser.dateTimeFormatInstance
-        this.activated = authUser.activated
-        this.enabled = authUser.enabled
-        this.roles = authUser.roles
-    }
-
-    public getAuthUser(): AuthUser {
-        return {
-            id: this.id,
-            username: this.username,
-            email: this.email,
-            firstName: this.firstName,
-            lastName: this.lastName,
-            locale: this.locale,
-            timeZone: this.timeZone,
-            dateTimeFormat: this.dateTimeFormat,
-            dateTimeFormatInstance: this.dateTimeFormatInstance,
-            activated: this.activated,
-            enabled: this.enabled,
-            roles: this.roles
-        }
-    }
-
-    public isAdmin(): boolean {
-        return this.roles && this.roles.includes("ROLE_ADMIN")
-    }
-
-    public isDeveloper(): boolean{
-        return this.roles && this.roles.includes("ROLE_DEVELOPER")
-    }
-
-    public isOnlyUser(): boolean {
-        return this.roles && this.roles.length === 2 && this.roles.includes("ROLE_USER") && this.roles.includes("ROLE_ANONYMOUS")
+        this.authUser = authUser
+        this.authUser.isAdmin = authUser.roles && authUser.roles.includes("ROLE_ADMIN")
+        this.authUser.isDeveloper = authUser.roles && authUser.roles.includes("ROLE_DEVELOPER")
+        this.authUser.isOnlyUser = authUser.roles && authUser.roles.length === 2
+            && authUser.roles.includes("ROLE_USER") && authUser.roles.includes("ROLE_ANONYMOUS")
     }
 }
 
