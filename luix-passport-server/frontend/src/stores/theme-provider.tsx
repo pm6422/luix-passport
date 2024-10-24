dc45d9005b00da5a083924ea5ea4fe174d419ff5
pm6422 <pm6422@126.com>
2024-10-24 09:26:13 +0200

    refactor

--- luix-passport-server/frontend/src/stores/theme-provider.tsx
+++ luix-passport-server/frontend/src/stores/theme-provider.tsx
@@ -22,7 +22,7 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
 
 export function ThemeProvider({
   children,
-  defaultTheme = "dark",
+  defaultTheme = "light",
   storageKey = "ui-theme",
   ...props
 }: ThemeProviderProps) {
