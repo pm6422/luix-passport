390e1693b1be4b59dbb89d71604d895698a05640
pm6422 <pm6422@126.com>
2025-04-23 17:07:59 +0200

    refactor

--- luix-passport-server/frontend/src/components/notification-nav.tsx
+++ luix-passport-server/frontend/src/components/notification-nav.tsx
@@ -4,8 +4,8 @@ import { IconBellRinging } from "@tabler/icons-react"
 export function NotificationNav() {
 
   return (
-    <Button variant="outline" className="animate-pulse relative size-12 rounded-full p-1 bg-gray-200 border-0">
-      <IconBellRinging className="text-red-500"></IconBellRinging>
+    <Button variant="outline" className="animate-pulse relative size-12 rounded-full p-1 border-0">
+      <IconBellRinging className="text-red-500 size-8"></IconBellRinging>
     </Button>
   )
 }
