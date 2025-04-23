c12312246839c2329e08661fe28d568aabbd6d24
pm6422 <pm6422@126.com>
2025-04-23 16:59:49 +0200

    refactor

--- luix-passport-server/frontend/src/components/custom/form-errors.tsx
+++ luix-passport-server/frontend/src/components/custom/form-errors.tsx
@@ -32,7 +32,7 @@ const FormErrors = ({
   return (
     Object.values(form.formState.errors).length > 0 && (
       <Alert variant={variant}>
-        <AlertTitle className="flex items-center"><IconExclamationCircle className="size-5 me-1" />Please check your input.</AlertTitle>
+        <AlertTitle className="flex items-center mb-3"><IconExclamationCircle className="size-5 me-1" />There was errors in your form, please check your inputs and try again.</AlertTitle>
         { errors && errors.map(err => <p className="ms-1">- {err.field}: {err.message}</p>) }
         {children && <AlertDescription className="pr-8 font-light">{children}</AlertDescription>}
       </Alert>
