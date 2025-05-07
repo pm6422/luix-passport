7705db2b0a353ac5bfb1c158b13bf97b559429d9
pm6422 <pm6422@126.com>
2025-05-07 10:57:33 +0200

    refactor

--- luix-passport-server/frontend/src/views/auth/reset-password.tsx
+++ luix-passport-server/frontend/src/views/auth/reset-password.tsx
@@ -62,7 +62,7 @@ export default function ForgotPassword() {
   return (
     <>
       <div className="container grid h-svh flex-col items-center justify-center bg-primary-foreground lg:max-w-none lg:px-0">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[480px] lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[600px] lg:p-8">
           <div className='mb-4 flex items-center justify-center'>
             <img
               alt='Logo'
