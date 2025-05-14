7bf96cf8adc8baff99ac864b14fef8843c33dfe7
pm6422 <pm6422@126.com>
2025-05-14 22:33:58 +0200

    refactor

--- luix-passport-server/frontend/src/views/account-settings/notifications/notifications-form.tsx
+++ luix-passport-server/frontend/src/views/account-settings/notifications/notifications-form.tsx
@@ -178,7 +178,7 @@ export function NotificationsForm() {
                 <FormDescription>
                   You can manage your mobile notifications in the{" "}
                   <Link
-                    to="/settings"
+                    to="/account-settings"
                     className="underline decoration-dashed underline-offset-4 hover:decoration-solid"
                   >
                     mobile settings
