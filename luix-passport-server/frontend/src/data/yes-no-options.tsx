38ad2043f889accfb4d3eff625708a52aa89cc75
pm6422 <pm6422@126.com>
2025-05-17 14:13:10 +0200

    refactor

---
+++ luix-passport-server/frontend/src/data/yes-no-options.tsx
@@ -0,0 +1,19 @@
+
+export const yesNoOptions = [
+  {
+    value: "true",
+    label: "Yes",
+    icon: undefined
+  },
+  {
+    value: "false",
+    label: "No",
+    icon: undefined
+  },
+  {
+    value: null,
+    label: "All",
+    icon: undefined
+  }
+]
+
