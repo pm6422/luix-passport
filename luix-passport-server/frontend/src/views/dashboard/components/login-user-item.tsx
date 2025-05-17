e77f669421fc8b4609d699fb6df4255caa2e4b4e
pm6422 <pm6422@126.com>
2025-05-17 15:14:08 +0200

    refactor

--- luix-passport-server/frontend/src/views/dashboard/components/login-user-item.tsx
+++ luix-passport-server/frontend/src/views/dashboard/components/login-user-item.tsx
@@ -4,20 +4,18 @@ import { DateTime } from "@/components/custom/date-time"
 
 export function LoginUserItem(loginUser : LoginUser) {
     return (
-        <div className="flex flex-col sm:flex-row gap-2">
-            <div className="flex items-center w-full sm:w-auto">
-              <Avatar className="size-9">
-                <AvatarImage src={"api/user-profile-pics/" + loginUser.id} alt="profile" />
-                <AvatarFallback>AVATAR</AvatarFallback>
-              </Avatar>
-              <div className="ml-4 space-y-1">
-                <p className="text-sm font-medium leading-none">{loginUser.firstName} {loginUser.lastName}</p>
-                <p className="text-xs text-muted-foreground">
-                  {loginUser.email}
-                </p>
-              </div>
+        <div className="flex items-center">
+            <Avatar className="size-9">
+              <AvatarImage src={"api/user-profile-pics/" + loginUser.id} alt="profile" />
+              <AvatarFallback>AVATAR</AvatarFallback>
+            </Avatar>
+            <div className="ml-4 space-y-1">
+              <p className="text-sm font-medium leading-none">{loginUser.firstName} {loginUser.lastName}</p>
+              <p className="text-xs text-muted-foreground">
+                {loginUser.email}
+              </p>
             </div>
-            <div className="ml-auto w-full sm:w-auto"><DateTime value={loginUser.signInAt} className="text-sm text-muted-foreground"/></div>
+            <div className="ml-auto"><DateTime value={loginUser.signInAt} className="text-xs sm:text-sm text-muted-foreground"/></div>
         </div>
     )
 }
