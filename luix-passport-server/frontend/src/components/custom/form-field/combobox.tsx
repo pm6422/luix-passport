1ff62e1c566cf04cdfdbdc0acb672bcc425c3f24
pm6422 <pm6422@126.com>
2025-05-18 10:46:00 +0200

    refactor

---
+++ luix-passport-server/frontend/src/components/custom/form-field/combobox.tsx
@@ -0,0 +1,120 @@
+import type { Control, FieldValues, Path } from "react-hook-form"
+import { FormControl, FormDescription, FormField, FormItem, FormMessage } from "@/components/ui/form"
+import { RequiredFormLabel } from "../required-form-label"
+import * as React from "react"
+import { Check, ChevronsUpDown } from "lucide-react"
+import { Key } from "react"
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+export type Option = {
+  label: string
+  value: string
+  icon?: React.ComponentType<{ className?: string }>
+}
+
+interface Props<TFieldValues extends FieldValues> {
+  control: Control<TFieldValues>
+  name: keyof TFieldValues
+  key?: Key
+  label?: string
+  options: Array<Option>
+  description?: string
+  placeholder?: string
+  required?: boolean
+  disabled?: boolean
+  formItemClassName?: string
+  hide?: boolean
+}
+
+const MultiSelectFormField = <TFieldValues extends FieldValues>({
+  control,
+  name,
+  key,
+  label,
+  options,
+  description,
+  placeholder,
+  required,
+  disabled,
+  formItemClassName,
+  hide = false
+}: Props<TFieldValues>) => {
+  const [open, setOpen] = React.useState(false)
+  const [value, setValue] = React.useState("")
+
+  return ( !hide &&
+    <FormField
+      control={disabled ? undefined : control}
+      name={name as Path<TFieldValues>}
+      key={key}
+      render={({ field }) => (
+        <FormItem className={formItemClassName}>
+          {label && <RequiredFormLabel required={required}>{label}</RequiredFormLabel>}
+          <FormControl>
+            <Popover open={open} onOpenChange={setOpen}>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  role="combobox"
+                  aria-expanded={open}
+                  className="w-[200px] justify-between"
+                >
+                  {value
+                    ? options.find((option) => option.value === field.value)?.label
+                    : "Select ..."}
+                  <ChevronsUpDown className="opacity-50" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-[200px] p-0">
+                <Command>
+                  <CommandInput placeholder={placeholder} className="h-9" />
+                  <CommandList>
+                    <CommandEmpty>No framework found.</CommandEmpty>
+                    <CommandGroup>
+                      {options.map((option) => (
+                        <CommandItem
+                          key={option.value}
+                          value={option.value}
+                          onSelect={(currentValue) => {
+                            setValue(currentValue === value ? "" : currentValue)
+                            setOpen(false)
+                          }}
+                        >
+                          {option.label}
+                          <Check
+                            className={cn(
+                              "ml-auto",
+                              value === option.value ? "opacity-100" : "opacity-0"
+                            )}
+                          />
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </CommandList>
+                </Command>
+              </PopoverContent>
+            </Popover>
+          </FormControl>
+          {description && <FormDescription>{description}</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
+
+export default MultiSelectFormField
