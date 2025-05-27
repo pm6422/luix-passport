dc54bf55c3db0e4813477167da3f7b6fe9c4eb3f
pm6422 <pm6422@126.com>
2025-05-27 11:37:41 +0200

    refactor

--- luix-passport-server/src/main/java/cn/luixtech/passport/server/service/impl/UserRoleServiceImpl.java
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/service/impl/UserRoleServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
