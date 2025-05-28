b2f8d15bbbd9bc60951bf4d16b518ec70ecd0d9e
pm6422 <pm6422@126.com>
2025-05-28 20:07:26 +0200

    refactor

--- luix-passport-server/frontend/src/domains/role.ts
+++ luix-passport-server/frontend/src/domains/role.ts
@@ -3,6 +3,7 @@ import { z } from "zod"
 export const roleSchema = z.object({
   id: z.string().trim().min(1, { message: "Required" }),
   remark: z.string().optional().nullable(),
+  permissionIds: z.array(z.string()).min(1, { message: "Required" }),
   createdAt: z.string().optional().nullable(),
   modifiedAt: z.string().optional().nullable()
 })
--- luix-passport-server/frontend/src/services/role-service.ts
+++ luix-passport-server/frontend/src/services/role-service.ts
@@ -6,6 +6,10 @@ export class RoleService {
   constructor() {
   }
 
+  public static find(reqParams: object): Promise<AxiosResponse<Array<Role>>> {
+    return http.get("api/roles", { params: reqParams })
+  }
+
   public static findAllIds(): Promise<AxiosResponse<Array<string>>> {
     return http.get("api/roles/ids")
   }
@@ -15,7 +19,7 @@ export class RoleService {
   }
 
   public static save(model: Role): Promise<void> {
-    return http.post("api/roles", model)
+    return model.id ? http.put("api/roles", model) : http.post("api/roles", model)
   }
 
   public static deleteById(id: string): Promise<void> {
