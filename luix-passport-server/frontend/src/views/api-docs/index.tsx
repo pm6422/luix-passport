4b3103b8f60528a3978591d04490c3d01d928b6d
pm6422 <pm6422@126.com>
2025-05-30 21:49:50 +0200

    refactor

--- luix-passport-server/frontend/src/views/api-docs/index.tsx
+++ luix-passport-server/frontend/src/views/api-docs/index.tsx
@@ -3,7 +3,7 @@ import { Card } from "@/components/ui/card"
 export default function ApiDocs() {
   return (
     <Card>
-      <iframe src="swagger-ui/index.html" width="100%" height="900"></iframe>
+      <iframe src="/swagger-ui/index.html" width="100%" height="900"></iframe>
     </Card>
   )
 }
