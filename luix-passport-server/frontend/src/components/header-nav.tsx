fc993bb0dae3e52e9d2510fb8b9c69c2b59f4827
pm6422 <pm6422@126.com>
2025-05-31 17:00:29 +0200

    refactor

--- luix-passport-server/frontend/src/components/header-nav.tsx
+++ luix-passport-server/frontend/src/components/header-nav.tsx
@@ -1,7 +1,12 @@
 import { Button } from "@/components/custom/button"
 import { Link } from "react-router-dom"
+import { AccountNav } from "@/components/account-nav"
+import { useStore } from "exome/react"
+import { loginUserStore } from "@/stores/login-user-store"
+import { isEmpty } from "lodash"
 
 export function HeaderNav() {
+  const { loginUser } = useStore(loginUserStore)
 
   return (
     <header className="flex justify-between items-center mt-5">
@@ -15,9 +20,13 @@ export function HeaderNav() {
         <Link to="/docs" className="text-sm font-medium hover:text-primary">
           Docs
         </Link>
-        <Button variant="outline" className="rounded-2xl">
-          <Link to="/login">Sign In</Link>
-        </Button>
+        {isEmpty(loginUser) ? (
+          <Button variant="outline" className="rounded-2xl">
+            <a href="/login">Sign In</a>
+          </Button>
+        ) : (
+          <AccountNav />
+        )}
       </nav>
     </header>
   )
