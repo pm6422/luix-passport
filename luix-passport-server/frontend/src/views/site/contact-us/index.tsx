65becba84b9c699c3ee9b7fbf379e399314b4499
pm6422 <pm6422@126.com>
2025-06-03 16:24:53 +0200

    refactor

--- luix-passport-server/frontend/src/views/site/contact-us/index.tsx
+++ luix-passport-server/frontend/src/views/site/contact-us/index.tsx
@@ -176,7 +176,7 @@ export default function ContactUs() {
                     d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                   />
                 </svg>
-                <span>helpdesk@luixtech.cn</span>
+                <span>support@luixtech.cn</span>
               </div>
               <div className="flex items-center">
                 <svg
