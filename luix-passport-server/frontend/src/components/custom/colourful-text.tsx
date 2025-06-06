397bd4cafb8e6f224de805c81dbdd529f8ac643c
pm6422 <pm6422@126.com>
2025-06-06 10:16:43 +0200

    refactor

--- luix-passport-server/frontend/src/components/custom/colourful-text.tsx
+++ luix-passport-server/frontend/src/components/custom/colourful-text.tsx
@@ -4,16 +4,16 @@ import { motion } from "motion/react";
 
 export default function ColourfulText({ text }: { text: string }) {
   const colors = [
-    "rgb(131, 179, 32)",
-    "rgb(47, 195, 106)",
-    "rgb(42, 169, 210)",
-    "rgb(4, 112, 202)",
-    "rgb(107, 10, 255)",
-    "rgb(183, 0, 218)",
-    "rgb(218, 0, 171)",
-    "rgb(230, 64, 92)",
-    "rgb(232, 98, 63)",
-    "rgb(249, 129, 47)",
+    "rgb(224, 187, 228)",  // #E0BBE4
+    "rgb(200, 158, 196)",  // #C89EC4
+    "rgb(171, 114, 176)",  // #AB72B0
+    "rgb(142, 68, 173)",   // #8E44AD
+    "rgb(108, 52, 131)",   // #6C3483
+    "rgb(224, 187, 228)",  // #E0BBE4
+    "rgb(200, 158, 196)",  // #C89EC4
+    "rgb(171, 114, 176)",  // #AB72B0
+    "rgb(142, 68, 173)",   // #8E44AD
+    "rgb(108, 52, 131)"    // #6C3483
   ];
 
   const [currentColors, setCurrentColors] = React.useState(colors);
