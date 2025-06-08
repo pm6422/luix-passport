9cee72ed4caa943312200d844029126b5112c617
pm6422 <pm6422@126.com>
2025-06-08 12:11:22 +0200

    upgrade to tailwind 4

--- luix-passport-server/frontend/src/views/account/appearance/appearance-form.tsx
+++ luix-passport-server/frontend/src/views/account/appearance/appearance-form.tsx
@@ -54,7 +54,7 @@ export function AppearanceForm() {
               <RadioGroup
                 onValueChange={field.onChange}
                 defaultValue={field.value}
-                className="grid max-w-md grid-cols-2 gap-8 pt-2"
+                className="grid max-w-xs grid-cols-2 gap-4 pt-2 md:max-w-md md:gap-8"
               >
                 <FormItem>
                   <FormLabel className="[&:has([data-state=checked])>div>div]:border-primary">
@@ -63,18 +63,18 @@ export function AppearanceForm() {
                     </FormControl>
                     <div className="flex flex-col items-center">
                       <div className="items-center rounded-md border-2 border-muted p-1 hover:border-accent">
-                        <div className="space-y-2 rounded-sm bg-[#ecedef] p-2">
-                          <div className="space-y-2 rounded-md bg-white p-2 shadow-xs">
-                            <div className="h-2 w-[80px] rounded-lg bg-[#ecedef]" />
-                            <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
+                        <div className="space-y-2 rounded-sm bg-[#ecedef] p-1 md:p-2">
+                          <div className="space-y-2 rounded-md bg-white p-1 shadow-xs md:p-2">
+                            <div className="h-2 w-[60px] rounded-lg bg-[#ecedef] md:w-[80px]" />
+                            <div className="h-2 w-[80px] rounded-lg bg-[#ecedef] md:w-[100px]" />
                           </div>
-                          <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-xs">
+                          <div className="flex items-center space-x-2 rounded-md bg-white p-1 shadow-xs md:p-2">
                             <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
-                            <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
+                            <div className="h-2 w-[80px] rounded-lg bg-[#ecedef] md:w-[100px]" />
                           </div>
-                          <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-xs">
+                          <div className="flex items-center space-x-2 rounded-md bg-white p-1 shadow-xs md:p-2">
                             <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
-                            <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
+                            <div className="h-2 w-[80px] rounded-lg bg-[#ecedef] md:w-[100px]" />
                           </div>
                         </div>
                       </div>
@@ -89,18 +89,18 @@ export function AppearanceForm() {
                     </FormControl>
                     <div className="flex flex-col items-center">
                       <div className="items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground">
-                        <div className="space-y-2 rounded-sm bg-slate-950 p-2">
-                          <div className="space-y-2 rounded-md bg-slate-800 p-2 shadow-xs">
-                            <div className="h-2 w-[80px] rounded-lg bg-slate-400" />
-                            <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
+                        <div className="space-y-2 rounded-sm bg-slate-950 p-1 md:p-2">
+                          <div className="space-y-2 rounded-md bg-slate-800 p-1 shadow-xs md:p-2">
+                            <div className="h-2 w-[60px] rounded-lg bg-slate-400 md:w-[80px]" />
+                            <div className="h-2 w-[80px] rounded-lg bg-slate-400 md:w-[100px]" />
                           </div>
-                          <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-xs">
+                          <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-1 shadow-xs md:p-2">
                             <div className="h-4 w-4 rounded-full bg-slate-400" />
-                            <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
+                            <div className="h-2 w-[80px] rounded-lg bg-slate-400 md:w-[100px]" />
                           </div>
-                          <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-xs">
+                          <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-1 shadow-xs md:p-2">
                             <div className="h-4 w-4 rounded-full bg-slate-400" />
-                            <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
+                            <div className="h-2 w-[80px] rounded-lg bg-slate-400 md:w-[100px]" />
                           </div>
                         </div>
                       </div>
