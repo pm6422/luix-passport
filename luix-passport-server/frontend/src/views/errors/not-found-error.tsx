9a82c888c8c54746312aac823db1cdc5ec6d3998
pm6422 <pm6422@126.com>
2025-06-12 11:16:44 +0200

    refactor

--- luix-passport-server/frontend/src/views/errors/not-found-error.tsx
+++ luix-passport-server/frontend/src/views/errors/not-found-error.tsx
@@ -9,7 +9,7 @@ export default function NotFoundError() {
         <h1 className="text-[7rem] font-bold leading-tight">404</h1>
         <span className="font-medium">Oops! Page Not Found!</span>
         <p className="text-center text-muted-foreground">
-          It seems like the page you"re looking for <br />
+          It seems like the page you're looking for <br />
           does not exist or might have been removed.
         </p>
         <div className="mt-6 flex gap-4">
