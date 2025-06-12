99b30e9135a315bc4fa83d5e2a322b720dd64009
pm6422 <pm6422@126.com>
2025-06-12 14:16:38 +0200

    refactor oauth2 client scope

--- luix-passport-resource-server/src/main/java/cn/luixtech/passport/resourceserver/controller/MessagesController.java
+++ luix-passport-resource-server/src/main/java/cn/luixtech/passport/resourceserver/controller/MessagesController.java
@@ -3,6 +3,7 @@ package cn.luixtech.passport.resourceserver.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,7 +11,7 @@ import java.util.List;
 public class MessagesController {
 
     @GetMapping("/api/resource-server-messages")
-    public List<String> getResourceServerMessages() {
-        return Arrays.asList("Resource Server Message 1", "Resource Server Message 2", "Resource Server Message 3");
+    public List<String> getResourceServerMessages(Principal principal) {
+        return Arrays.asList("authorization code mode can get user info: " + principal.getName(), "Resource Server Message 1", "Resource Server Message 2", "Resource Server Message 3");
     }
 }
