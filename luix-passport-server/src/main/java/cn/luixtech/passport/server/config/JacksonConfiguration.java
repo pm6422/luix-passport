803e1113a4ba8b5b5e11373c30f508d4fa84342a
pm6422 <pm6422@126.com>
2025-06-16 15:57:03 +0200

    refactor

---
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/config/JacksonConfiguration.java
@@ -0,0 +1,19 @@
+package cn.luixtech.passport.server.config;
+
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfiguration {
+    @Bean
+    public JavaTimeModule javaTimeModule() {
+        return new JavaTimeModule();
+    }
+
+    @Bean
+    public Jdk8Module jdk8TimeModule() {
+        return new Jdk8Module();
+    }
+}
