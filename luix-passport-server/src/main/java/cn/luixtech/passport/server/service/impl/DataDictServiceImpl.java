64f11b3f61fae37e5ea2177dd40de5390b98ec6a
pm6422 <pm6422@126.com>
2025-06-16 17:00:53 +0200

    refactor

--- luix-passport-server/src/main/java/cn/luixtech/passport/server/service/impl/DataDictServiceImpl.java
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/service/impl/DataDictServiceImpl.java
@@ -50,7 +50,7 @@ public class DataDictServiceImpl implements DataDictService {
      * Execute every day at 2:00 AM
      */
 //    @Scheduled(initialDelay = 0, fixedRate = 300000)
-    @Scheduled(cron = "0 0 17 * * *")
+    @Scheduled(cron = "0 0 2 * * *")
     @SchedulerLock(name = "updateTimezoneUtcOffset")
     @SchedulerExecutionLog(name = "updateTimezoneUtcOffset")
     @Override
