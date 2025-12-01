dacf670053c3abbf2a3de243b740667dc0077682
Louis <pm6422@126.com>
2025-12-01 14:08:23 +0100

    refactor(statemachine): make StateMachineListener generic for type safety

--- luix-passport-server/src/main/java/cn/luixtech/passport/server/statemachine/StateMachineListener.java
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/statemachine/StateMachineListener.java
@@ -5,10 +5,10 @@ import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
 
 @Slf4j
-public class StateMachineListener extends StateMachineListenerAdapter {
+public class StateMachineListener<S, E> extends StateMachineListenerAdapter<S, E> {
  
     @Override
-    public void stateChanged(State from, State to) {
+    public void stateChanged(State<S, E> from, State<S, E> to) {
         System.out.printf("Transitioned from %s to %s%n", from == null ?
           "none" : from.getId(), to.getId());
     }
