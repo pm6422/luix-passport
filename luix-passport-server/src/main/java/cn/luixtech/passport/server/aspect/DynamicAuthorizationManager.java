4413c1817a3be1970fcc9a7e5766ab4417b885ae
Louis <pm6422@126.com>
2025-12-02 11:46:34 +0100

    refactor: deprecate `DynamicAuthorizationManager#authorize` method and reformat Javadoc link.

--- luix-passport-server/src/main/java/cn/luixtech/passport/server/aspect/DynamicAuthorizationManager.java
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/aspect/DynamicAuthorizationManager.java
@@ -10,14 +10,18 @@ import org.springframework.stereotype.Component;
 import java.util.function.Supplier;
 
 /**
- * Refer to <a href="https://docs.spring.io/spring-security/reference/servlet/authorization/method-security.html">Using a Custom Authorization Manager</a>
+ * Refer to <a href=
+ * "https://docs.spring.io/spring-security/reference/servlet/authorization/method-security.html">Using
+ * a Custom Authorization Manager</a>
  */
 @Component
 @Deprecated
 @Slf4j
 public class DynamicAuthorizationManager implements AuthorizationManager<MethodInvocation> {
     @Override
-    public AuthorizationDecision authorize(Supplier<? extends Authentication> authentication, MethodInvocation invocation) {
+    @Deprecated
+    public AuthorizationDecision authorize(Supplier<? extends Authentication> authentication,
+            MethodInvocation invocation) {
         return new AuthorizationDecision(true);
     }
 }
